11b2c15c6e119badc474412d22e7d81aa0d5037e
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-01 18:22:01 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,22 +1,32 @@
 class GenreController < ApplicationController
 
   def show
-
+    @genre = Genre.find(params[:id])
   end
 
   def new
-
+    @genre = Genre.find(params[:id])
   end
 
   def edit
-
+    @genre = Genre.find(params[:id])
   end
 
   def create
-
+    @genre = Genre.new(post_params)
+    @genre.save
+    redirect_to genre_path(@genre)
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(post_params)
+    redirect_to genre_path(@genre)
+  end
 
+  private
+
+  def post_params
+    params.require(:genre).permit(:name)
   end
 end
