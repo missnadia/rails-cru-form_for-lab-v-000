56514ec0717c80cfcbba664322a985880de21190
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-01 18:11:48 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :artist, except: :destroy
+  resources :artists, except: :destroy
+  resources :songs, except: :destroy
+  resources :genres, except: :destroy
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
